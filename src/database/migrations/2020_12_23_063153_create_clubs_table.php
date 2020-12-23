81c68103babbcf085f3395357e67d33beb1886c5
Yavuz Can Tekin ORALTAY <yavuzcantekinoraltay@Yavuz-MacBook-Pro.local>
2020-12-23 09:38:31 +0300

    Migrations Created.

---
+++ src/database/migrations/2020_12_23_063153_create_clubs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->integer('goals_for')->default(0);
+            $table->integer('goals_against')->default(0);
+            $table->integer('points')->default(0);
+            $table->integer('power_rank')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubs');
+    }
+}
